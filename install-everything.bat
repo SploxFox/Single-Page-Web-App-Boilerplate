17c0b29f6c67fa28fa57d0fbd13c03ade803279e
SploxFox <sploxfox@gmail.com>
2019-08-31 14:18:09 -0500

    Fixed problem where only first command was executing

--- install-everything.bat
+++ install-everything.bat
@@ -1,4 +1,4 @@
-npm i webpack webpack-cli --save-dev
-npm i typescript ts-loader --save-dev
-npm i react react-dom --save
-npm i @types/react @types/react-dom --save
+CALL npm i webpack webpack-cli --save-dev
+CALL npm i typescript ts-loader --save-dev
+CALL npm i react react-dom --save
+CALL npm i @types/react @types/react-dom --save
