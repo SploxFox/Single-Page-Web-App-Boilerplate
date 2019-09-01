ba588a2c506308bb14d833716af0b36a4dc1730e
SploxFox <sploxfox@gmail.com>
2019-08-31 19:00:42 -0500

    Added node types

--- install-everything.bat
+++ install-everything.bat
@@ -2,3 +2,4 @@ CALL npm i webpack webpack-cli --save-dev
 CALL npm i typescript ts-loader --save-dev
 CALL npm i react react-dom --save
 CALL npm i @types/react @types/react-dom --save
+CALL npm i @types/node
